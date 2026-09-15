925eb6c831384d4363333a1bb3f7e12ac37e8572
Nur Ikhwanudin <me.ikhwanudin@gmail.com>
2026-09-15 12:14:59 +0700

    feat(Auth): IMPL basic Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AuthResource;
+use Dedoc\Scramble\Attributes\PathParameter;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    /**
+     * Register
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+
+    public function register(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
+
+    /**
+     * Login
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        // Revoke old tokens for single-device login
+        $user->tokens()->delete();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    /**
+     * Logout
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Successfully logged out'
+        ]);
+    }
+
+    /**
+     * Get specific user by id
+     *
+     * @param Request $request
+     * @return mixed
+     */
+
+    #[PathParameter('id', description: 'user id', type: 'string', format: 'ulid', example: '01m2g290bhpdyb7jf8d35n8ej5')]
+    public function show($id)
+    {
+        if(Auth::id() !== $id){
+            return response()->json([
+                'message' => 'Access denied. You do not have permission to access this resource.'
+            ], 403);
+        }
+
+        $user = User::where('id', $id)->firstOrFail();
+
+        return response()->json([
+            'id' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::prefix('posts')->group(function () {
+    Route::get('/', [PostController::class, 'index']);
+    Route::get('{post}', [PostController::class, 'show']);
+});
+
+// Authenticated Routes
+Route::middleware(['auth:sanctum'])->group(function (){
+    Route::prefix('posts')->group(function (){
+        Route::patch('{post}', [PostController::class, 'update']);
+        Route::post('/', [PostController::class, 'store']);
+        Route::delete('/', [PostController::class, 'destroy']);
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/users/{id}', [AuthController::class, 'show']);
+});
